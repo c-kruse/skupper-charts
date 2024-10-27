2222cf6f4295ee6e71d6136b023aa30c9e0591eb
Christian Kruse <christian@c-kruse.com>
2024-10-26 21:17:53 -0700

    upgrade crds for v2.0.0-preview-2
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- hack/fetch-crds.sh
+++ hack/fetch-crds.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-TAG="${1:-"2.0.0-preview-1"}"
-curl -Ls "https://github.com/skupperproject/skupper/archive/refs/tags/${TAG}.tar.gz" > archive.tar.gz
+set -x
+
+TAG="${1:-"2.0.0-preview-2"}"
+ARCHIVE_REF="${ARCHIVE_REF:-https://github.com/skupperproject/skupper/archive/refs/tags/${TAG}.tar.gz}"
+curl -Ls "${ARCHIVE_REF}" > archive.tar.gz
 
 BASEPATH="skupper-$TAG/api/types/crds"
 DISTPATH=./skupper/charts/crds/crds
 rm -f "${DISTPATH}/*"
 
-tar -xvzf archive.tar.gz -C "$DISTPATH" --strip-components=4 \
-  "${BASEPATH}/skupper_access_grant_crd.yaml" \
-  "${BASEPATH}/skupper_access_token_crd.yaml" \
-  "${BASEPATH}/skupper_attached_connector_anchor_crd.yaml" \
-  "${BASEPATH}/skupper_attached_connector_crd.yaml" \
-  "${BASEPATH}/skupper_certificate_crd.yaml" \
-  "${BASEPATH}/skupper_connector_crd.yaml" \
-  "${BASEPATH}/skupper_link_crd.yaml" \
-  "${BASEPATH}/skupper_listener_crd.yaml" \
-  "${BASEPATH}/skupper_router_access_crd.yaml" \
-  "${BASEPATH}/skupper_secured_access_crd.yaml" \
-  "${BASEPATH}/skupper_site_crd.yaml"
-
+tar -xvzf archive.tar.gz -C "$DISTPATH" --strip=4 "${BASEPATH}"
 
 rm -rf archive.tar.gz
